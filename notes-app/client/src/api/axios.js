32e026938831b06d53281b78a6f9ffbe552799b0
suhani2406 <suhanisunitiwari@gmail.com>
2026-05-06 13:21:24 +0530

    fix api baseURL

--- notes-app/client/src/api/axios.js
+++ notes-app/client/src/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: ""https://final-project-3-qemw.onrender.com""
+  baseURL: "https://final-project-3-qemw.onrender.com/api",
 });
-
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
